379da9568791e26c7e2f0f41fa6fe6aaa8a54ced
Prohalladdas <prolladdas@gmail.com>
2025-03-22 21:41:06 +0530

    Finishing a Quiz

--- src/component/NextButton.jsx
+++ src/component/NextButton.jsx
@@ -1,11 +1,23 @@
-export default function NextButton({ dispatch, answer }) {
+export default function NextButton({ dispatch, answer, index, numbQuestions }) {
   if (answer === null) return null;
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: "nextQuestion" })}
-    >
-      Next
-    </button>
-  );
+
+  if (index < numbQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numbQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
